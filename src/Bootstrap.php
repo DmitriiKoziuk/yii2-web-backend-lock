cfbe90f172ebf329b207b06718a72623d91f3c44
Kynica <dmitry.kozyuk@gmail.com>
2019-02-11 15:49:45 +0200

    Remove dependency from dmitriikoziuk/yii2-config-manager.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,25 +6,16 @@ use yii\helpers\Url;
 use yii\web\Application as WebApp;
 use yii\web\Controller;
 use yii\base\BootstrapInterface;
-use DmitriiKoziuk\yii2ConfigManager\ConfigManagerModule;
-use DmitriiKoziuk\yii2ConfigManager\services\ConfigService;
 
 final class Bootstrap implements BootstrapInterface
 {
     /**
      * @param \yii\base\Application $app
-     * @throws \yii\base\InvalidConfigException
-     * @throws \yii\di\NotInstantiableException
      */
     public function bootstrap($app)
     {
         if ($app instanceof WebApp) {
-            /** @var ConfigService $configService */
-            $configService = Yii::$container->get(ConfigService::class);
-            $backendAppId = $configService->getValue(
-                ConfigManagerModule::GENERAL_CONFIG_NAME,
-                'backendAppId'
-            );
+            $backendAppId = $app->params['backendAppId'] ?? 'app-backend';
             if ($backendAppId == $app->id) {
                 $app->on(Controller::EVENT_BEFORE_ACTION, function () use ($app) {
                     if (
